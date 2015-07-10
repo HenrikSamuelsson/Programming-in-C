6409b46e9f192f89f2efd437664ac83a41e67191
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-10 22:36:39 +0200

    Parts of exercise 2.1.

--- Exercises/Chapter_02/Exercise_01/Program_02_02/program_02_02.c
+++ Exercises/Chapter_02/Exercise_01/Program_02_02/program_02_02.c
@@ -1,11 +1,12 @@
 /*
- * program_02_01.c - Writing your first C program
+ * program_02_02.c
  */
 #include <stdio.h>
 
 int main (void)
 {
 	printf ("Programming is fun.\n");
+	printf ("And programming in C is even more fun.\n");
 
 	return 0;
 }
