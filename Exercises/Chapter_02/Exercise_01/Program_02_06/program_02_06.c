9c5a3bf257865119fab7e76b5f4994947f7a0301
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 15:29:05 +0200

    Part of exercise 2.6

---
+++ Exercises/Chapter_02/Exercise_01/Program_02_06/program_02_06.c
@@ -0,0 +1,23 @@
+/*
+ * program_02_06.c - Using comments in a program
+ */
+
+/* This program adds two integer values and displays the result */
+
+#include <stdio.h>
+
+int main (void)
+{
+	// Declare variables
+	int value1, value2, sum;
+
+	// Assign values and calculate their sum
+	value1 = 50;
+	value2 = 25;
+	sum = value1 + value2;
+
+	// Display the result
+	printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+
+	return 0;
+}
