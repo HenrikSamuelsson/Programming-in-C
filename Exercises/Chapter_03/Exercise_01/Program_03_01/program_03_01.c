5f0ddbccc51d312fc2329f90f444621d125d2714
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 23:32:01 +0200

    Part of exercise 3.1.

---
+++ Exercises/Chapter_03/Exercise_01/Program_03_01/program_03_01.c
@@ -0,0 +1,24 @@
+/*
+ * program_03_01.c - Using the basic data types
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int 	integerVar = 100;
+	float 	floatingVar = 331.79;
+	double	doubleVar = 8.44e+11;
+	char	charVar = 'W';
+
+	_Bool	boolVar = 0;
+
+	printf ("integerVar = %i\n", integerVar);
+	printf ("floatingVar = %f\n", floatingVar);
+	printf ("doubleVar = %e\n", doubleVar);
+	printf ("doubleVar = %g\n", doubleVar);
+	printf ("charVar = %c\n", charVar);
+
+	printf ("boolVar = %i\n", boolVar);
+
+	return 0;
+}
