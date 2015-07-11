1c0d3879a304aebc427a37fad03ab393a9b01be2
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 15:12:15 +0200

    Part of exercise 2.5.

---
+++ Exercises/Chapter_02/Exercise_01/Program_02_05/program_02_05.c
@@ -0,0 +1,16 @@
+/*
+ * program_02_05.c
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int value1, value2, sum;
+
+	value1 = 50;
+	value2 = 25;
+	sum = value1 + value2;
+	printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+
+	return 0;
+}
