c888d15b8c576feb757cb5b39e850b6395658b1c
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 14:50:52 +0200

    Part of exercise 2.1.

---
+++ Exercises/Chapter_02/Exercise_01/Program_02_04/program_02_04.c
@@ -0,0 +1,14 @@
+/*
+ * program_02_04.c - Displaying values
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int sum;
+
+	sum = 50 + 25;
+	printf ("The sum of 50 and 25 is %i\n", sum);
+
+	return 0;
+}
