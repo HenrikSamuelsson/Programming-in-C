a67036db46f809a2a06f23cf9274e05d790e0f6b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 17:19:09 +0200

    Source for exercise 2.4.

---
+++ Exercises/Chapter_02/Exercise_04/exercise_02_04.c
@@ -0,0 +1,19 @@
+/*
+ * exercise_02_04.c
+ *
+ * author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int value1, value2, difference;
+
+	value1 = 87;
+	value2 = 15;
+	difference = value1 - value2;
+
+	printf ("%i minus %i is %i\n", value1, value2, difference);
+
+	return 0;
+}
