10edc94c32cd1f3c7839ddce6d071e9e51aad679
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 16:56:03 +0200

    Source for exercise 2.3.

---
+++ Exercises/Chapter_02/Exercise_03/exercise_02_03.c
@@ -0,0 +1,15 @@
+/*
+ * exercise_02_03.c
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Testing...");
+	printf("....1");
+	printf("...2");
+	printf("..3");
+	printf("\n");
+
+	return 0;
+}
