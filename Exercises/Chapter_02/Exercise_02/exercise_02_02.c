45aac8c1e100711c7671c3544b071df9b56ddf08
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 16:21:55 +0200

    Source code for exercise 2.2.

---
+++ Exercises/Chapter_02/Exercise_02/exercise_02_02.c
@@ -0,0 +1,14 @@
+/*
+ * exercise_02_02.c
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	printf ("In C, lowercase letters are significant.\n");
+	printf ("main() is where program execution begins.\n");
+	printf ("Opening and closing braces enclose program statements in a routine.\n");
+	printf ("All program statements must be terminated by a semicolon.\n");
+
+	return 0;
+}
