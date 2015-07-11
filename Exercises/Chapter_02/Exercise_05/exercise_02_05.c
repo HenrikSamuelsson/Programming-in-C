147b14fbd61080b3e64157bb36f883aa29d63782
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-11 22:07:07 +0200

    source code for exercise 2.5

---
+++ Exercises/Chapter_02/Exercise_05/exercise_02_05.c
@@ -0,0 +1,17 @@
+/*
+ * exercise_02_05.c
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int sum;
+
+	// compute result
+	sum = 25 + 37 - 19;
+
+	// display result
+	printf("The answer is %i\n", sum);
+
+	return 0;
+}
