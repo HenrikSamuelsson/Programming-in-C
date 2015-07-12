1b6db2af5a797113172cdd75fe3f57bca11ea61c
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-12 17:21:01 +0200

    Adding  program_03_02.c.

---
+++ Exercises/Chapter_03/Exercise_01/Program_03_05/program_03_05.c
@@ -0,0 +1,35 @@
+/*
+ * program_03_02.c - Converting Between Integers and Floats
+ */
+
+// Basic conversions in C
+#include <stdio.h>
+
+int main (void)
+{
+	float f1 = 123.125, f2;
+	int i1, i2 = -150;
+	char c = 'a';
+
+	// floating to integer conversion
+	i1 = f1;
+	printf ("%f assigned to an int procedure %i\n", f1, i1);
+
+	// integer to floating conversion
+	f1 = i2;
+	printf ("%i assigned to a float procedure %f\n", i2, f1);
+
+	// integer divided by integer
+	f1 = i2 / 100;
+	printf ("%i divided by 100 produces %f\n", i2, f1);
+
+	// integer divided by float
+	f2 = i2 / 100.0;
+	printf ("%i divided by 100.0 produces %f\n", i2, f2);
+
+	// type cast operator
+	f2 = (float) i2 / 100;
+	printf ("(float) %i divided by 100 produces %f\n", i2, f2);
+
+	return 0;
+}
