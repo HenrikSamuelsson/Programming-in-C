76ae14ba5e6044b9ded934d25297d179f77a9237
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-12 13:38:14 +0200

    Source code for program_03_02.c

---
+++ Exercises/Chapter_03/Exercise_01/Program_03_02/program_03_02.c
@@ -0,0 +1,31 @@
+/*
+ * program_03_02.c - Using the arithmetic operators
+ */
+
+// Illustrate the use of various arithmetic operators
+#include <stdio.h>
+
+int main (void)
+{
+	int a = 100;
+	int b = 2;
+	int c = 25;
+	int d = 4;
+	int result;
+
+	result = a - b;		// subtraction
+	printf ("a - b = %i\n", result);
+
+	result = b * c;		// multiplication
+	printf ("b * c = %i\n", result);
+
+	result = a / c;		// division
+	printf ("a / c = %i\n", result);
+
+	result = a + b * c;		// precedence
+	printf ("a + b * c = %i\n", result);
+
+	printf ("a * b + c * d = %i\n", a * b + c * d);
+
+	return 0;
+}
