56f4e7088c1902feafa7da7b3d331dccef8b3a68
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-12 17:46:34 +0200

    Adding exercise_03_02.c.

---
+++ Exercises/Chapter_03/Exercise_02/exercise_03_02.c
@@ -0,0 +1,26 @@
+/*
+ * exercise_03_02.c
+ *
+ * Test program to verify the answer for exercise 3.2.
+ *
+ * Author: Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+int main (void)
+{
+	int Int;
+	int Calloc;
+	int floaitng;
+	int ReInitialize;
+
+	// int char;
+	int Xx;
+	int _1312;
+	int _;
+
+	// int 6_05;
+	int alpha_beta_routine;
+	int z;
+	int A$;
+}
+
