6eb16db91fd0ad9b0fae6d3a427b6f664adb08c6
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-15 22:27:54 +0200

    program_02_01.c that is part of exercise 4.1.

---
+++ Exercises/Chapter_04/Exercise_01/Program_04_01/program_04_01.c
@@ -0,0 +1,19 @@
+/*
+ * program_02_01.c - Calculating the Eighth Triangular Number
+ */
+
+// Program to calculate the eighth triangular number
+
+#include <stdio.h>
+
+int main (void)
+{
+	int triangularNumber;
+
+	triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+	printf ("The eighth triangular number is %i\n", triangularNumber);
+
+	return 0;
+}
+
