65f01253381a688fddb40053a4ecbce6012a863b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-17 16:43:37 +0200

    Program 4.9

---
+++ Exercises/Chapter_04/Exercise_01/Program_04_09/program_04_09.c
@@ -0,0 +1,26 @@
+/*
+ * program_04_09.c - Revised Program to Reverse the Digits of a Number
+ *
+ * Program to reverse the digits of a number
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int number, right_digit;
+
+	printf ("Enter your number.\n");
+	scanf ("%i", &number);
+
+	do
+	{
+		right_digit = number % 10;
+		printf ("%i", right_digit);
+		number = number / 10;
+	}
+	while (number != 0);
+
+	printf ("\n");
+	return 0;
+}
