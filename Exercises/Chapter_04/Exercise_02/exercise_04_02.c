cc05ce395891d84a64dee5afb0cd719bbf1fdea9
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-17 18:50:36 +0200

    Exercise 4.2

---
+++ Exercises/Chapter_04/Exercise_02/exercise_04_02.c
@@ -0,0 +1,24 @@
+/*
+ * exercise_04_02.c
+ *
+ * Program that generates a table of squared numbers.
+ *
+ * Author: Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int n;
+
+	printf ("  n      n^2 \n");
+	printf ("-----   -----\n");
+
+	for ( n = 1; n < 11; n++)
+	{
+		printf(" %2i     %3i \n", n, n * n);
+	}
+
+	return 0;
+}
