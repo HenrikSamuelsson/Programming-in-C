e6e203b1b9f5b7a88a0bb8213d9fc98b75ccb608
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-20 07:28:21 +0200

    Exercise 4.5.

---
+++ Exercises/Chapter_04/Exercise_05/exercise_04_05.c
@@ -0,0 +1,25 @@
+/*
+ * exercise_04_05.c
+ *
+ * Author: Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int n, two_to_the_n;
+
+	printf("TABLE OF POWERS OF TWO\n\n");
+	printf(" n     2 to the n\n");
+	printf("---    ---------------\n");
+
+	two_to_the_n = 1;
+	for (n = 0; n <= 10; ++n)
+	{
+		printf("%2i           %i\n", n, two_to_the_n);
+		two_to_the_n *= 2;
+	}
+
+	return 0;
+}
