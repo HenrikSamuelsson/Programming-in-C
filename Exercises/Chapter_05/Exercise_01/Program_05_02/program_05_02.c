99a51570ba859a19d06fbe84d6b239f8c4c86f54
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-21 18:02:32 +0200

    program_05_02.c

---
+++ Exercises/Chapter_05/Exercise_01/Program_05_02/program_05_02.c
@@ -0,0 +1,37 @@
+/*
+ * program_05_02.c
+ *
+ * Program to calculate the average of a set of grades and count the number of
+ * failing grades.
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int numberOfGrades, i, grade;
+	int gradeTotal = 0;
+	int failureCount = 0;
+	float average;
+
+	printf ("How many grades will you be entering? ");
+	scanf ("%i", &numberOfGrades);
+
+	for (i = 1; i <= numberOfGrades; ++i)
+	{
+		printf ("Enter grade #%i", i);
+		scanf ("%i", &grade);
+
+		gradeTotal = gradeTotal + grade;
+
+		if (grade < 65)
+			++failureCount;
+	}
+
+	average = (float) gradeTotal / numberOfGrades;
+
+	printf("\nGrade average = %.2f\n", average);
+	printf("Number of failures = %i\n", failureCount);
+
+	return 0;
+}
