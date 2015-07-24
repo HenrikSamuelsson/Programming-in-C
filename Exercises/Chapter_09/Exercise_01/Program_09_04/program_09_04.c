ea6cfead9cde2a9b6db6c5071512e9274963f26a
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-24 08:33:39 +0200

    program_09_04.c

---
+++ Exercises/Chapter_09/Exercise_01/Program_09_04/program_09_04.c
@@ -0,0 +1,37 @@
+/*
+ * program_09_04.c - Testing strings for equality
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to determine if two strings are equal
+bool equalStrings (const char s1[], const char s2[])
+{
+	int i = 0;
+	bool areEqual;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		++i;
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+		areEqual = true;
+	else
+		areEqual = false;
+
+	return areEqual;
+}
+
+int main (void)
+{
+
+	bool equalStrings (const char s1[], const char s2[]);
+	const char stra[] = "string compare test";
+	const char strb[] = "string";
+
+	printf ("%i\n", equalStrings(stra, strb));
+	printf ("%i\n", equalStrings(stra, stra));
+	printf ("%i\n", equalStrings(strb, "string"));
+
+	return 0;
+}
