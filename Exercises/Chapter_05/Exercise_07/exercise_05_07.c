963442cea2edde76f58c36798f0cc23b304b8a85
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-27 08:18:28 +0200

    exercise_05_07.c

---
+++ Exercises/Chapter_05/Exercise_07/exercise_05_07.c
@@ -0,0 +1,32 @@
+/*
+ * program_05_07.c
+ *
+ * Program to generate a table of prime numbers.
+ *
+ * Author: Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main (void)
+{
+	int p, d;
+	bool isPrime;
+
+	for (p = 3; p <= 50; p += 2)
+	{
+		isPrime = true;
+
+		for (d = 2; d < p; ++d && isPrime)
+			if (p % d == 0)
+				isPrime = false;
+
+		if (isPrime != false)
+			printf ("%i ", p);
+	}
+
+	printf ("\n");
+
+	return 0;
+}
