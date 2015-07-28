658f9d939e4020287a911ae3517cf87cb02d64ca
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-28 08:56:04 +0200

    program_06_02.c

---
+++ Exercises/Chapter_06/Exercise_01/Program_06_02/program_06_02.c
@@ -0,0 +1,34 @@
+/*
+ * program_06_02.c - Demonstrating an Array of Counters
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int ratingCounters[11], i, response;
+
+	for (i = 1; i <= 10; ++i)
+		ratingCounters[i] = 0;
+
+	printf ("Enter your responses\n");
+
+	for (i = 1; i <= 20; ++i)
+	{
+		scanf ("%i", &response);
+
+		if (response < 1 || response > 10)
+			printf ("Bad response: %i\n", response);
+		else
+			++ratingCounters[response];
+	}
+
+	printf ("\n\n");
+	printf ("Rating   Number of Responses\n");
+	printf ("------   -------------------\n");
+
+	for (i = 1; i <= 10; ++i)
+		printf ("%4i%4i\n", i, ratingCounters[i]);
+
+	return 0;
+}
