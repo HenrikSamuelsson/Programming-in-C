e8efb081c26c8c7cb0879c869943629dbdd794e8
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-07-29 22:01:31 +0200

    program 11.1

---
+++ Exercises/Chapter_11/Exercise_01/Program_11_01/program_11_01.c
@@ -0,0 +1,19 @@
+/*
+ * program_11_01.c
+ *
+ * Program to demonstrate the bitwise AND operator.
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned int word1 = 077u, word2 = 0150u, word3 = 0210u;
+
+	printf ("%o  ", word1 & word2);
+	printf ("%o  ", word1 & word1);
+	printf ("%o  ", word1 & word2 & word3);
+	printf ("%o\n", word1 & 1);
+
+	return 0;
+}
