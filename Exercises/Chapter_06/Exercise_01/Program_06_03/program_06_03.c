84a6f3f394254ff4e4bc33a58bc58509c913be62
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-30 08:41:15 +0200

    program_06_03.c

---
+++ Exercises/Chapter_06/Exercise_01/Program_06_03/program_06_03.c
@@ -0,0 +1,23 @@
+/*
+ * program_06_03.c
+ *
+ * Program to generate the first 15 Fibonacci numbers.
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int Fibonacci[15], i;
+	
+	Fibonacci[0] = 0;	// by definiton
+	Fibonacci[1] = 1;	// by definiton
+	
+	for (i = 2; i < 15; ++i)
+		Fibonacci[i] = Fibonacci[i - 2] + Fibonacci[i - 1];
+		
+	for (i = 0; i < 15; ++i)
+		printf ("%i\n", Fibonacci[i]);
+	
+	return 0;
+}
