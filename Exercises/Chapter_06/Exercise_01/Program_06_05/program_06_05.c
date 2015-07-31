def4b11f332a2d1dfca34de991711eb25da3b619
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-07-31 08:46:59 +0200

    program_06_05.c

---
+++ Exercises/Chapter_06/Exercise_01/Program_06_05/program_06_05.c
@@ -0,0 +1,21 @@
+/*
+ * program_06_05.c - Initializing Arrays
+ *
+ * Program that illustrates twe types of array-initialization techniques.
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int array_values[10] = { 0, 1, 4, 9, 16 };
+	int i;
+
+	for (i = 5; i < 10; ++i)
+		array_values[i] = i * i;
+
+	for (i = 0; i < 10; ++i)
+		printf("array_values[%i] = %i\n",i , array_values[i]);
+
+	return 0;
+}
