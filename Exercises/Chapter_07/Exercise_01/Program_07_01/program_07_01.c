6feafe9744ec55699e25f4474704fc340c425f40
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-08-07 18:44:39 +0200

    Adding exercise 7.1.

---
+++ Exercises/Chapter_07/Exercise_01/Program_07_01/program_07_01.c
@@ -0,0 +1,17 @@
+/*
+ * program_07_01.c - Writing a Function in C
+ */
+
+#include <stdio.h>
+
+void printMessage (void)
+{
+	printf ("Programming is fun.\n");
+}
+
+int main (void)
+{
+	printMessage ();
+
+	return 0;
+}
