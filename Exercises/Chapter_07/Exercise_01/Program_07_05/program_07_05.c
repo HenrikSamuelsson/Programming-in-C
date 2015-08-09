8d926f9f533701dc2ccc4f9212a18c267bcfd2ea
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-08-09 11:36:18 +0200

    program_07_05.c

---
+++ Exercises/Chapter_07/Exercise_01/Program_07_05/program_07_05.c
@@ -0,0 +1,35 @@
+/*
+ * program_07_05.c
+ */
+
+#include <stdio.h>
+
+/*
+ * Function to find the greatest common divisor of two nonnegative integer
+ * values.
+ */
+
+void gcd (int u, int v)
+{
+	int temp;
+
+	printf ("The gcd of %i and %i is ", u, v);
+
+	while (v != 0)
+	{
+		temp = u % v;
+		u = v;
+		v = temp;
+	}
+
+	printf ("%i\n", u);
+}
+
+int main (void)
+{
+	gcd (150, 35);
+	gcd (1026, 405);
+	gcd (83, 240);
+
+	return 0;
+}
