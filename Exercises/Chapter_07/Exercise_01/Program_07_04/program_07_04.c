f94a426d4c85b70fcee626f31975e5c5ad1ac573
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-08-09 10:36:51 +0200

    program_07_04.c

---
+++ Exercises/Chapter_07/Exercise_01/Program_07_04/program_07_04.c
@@ -0,0 +1,25 @@
+/*
+ * program_07_04.c - Calculating the nth Triangular Number
+ */
+
+#include <stdio.h>
+
+/*  Function to calculate nth triangular number. */
+void calculateTriangularNumber (int n)
+{
+	int i, triangularNumber = 0;
+
+	for (i = 1; i <= n; ++i)
+		triangularNumber += i;
+
+	printf("Triangular number %i is %i\n", n, triangularNumber);
+}
+
+int main (void)
+{
+	calculateTriangularNumber (10);
+	calculateTriangularNumber (20);
+	calculateTriangularNumber (50);
+
+	return 0;
+}
