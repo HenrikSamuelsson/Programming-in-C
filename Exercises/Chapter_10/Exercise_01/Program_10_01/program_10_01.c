e4cf757c8b7e99803dab089e09ed6d99a87641c7
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-08-24 08:31:24 +0200

    Adds source code for Program 10.1.

---
+++ Exercises/Chapter_10/Exercise_01/Program_10_01/program_10_01.c
@@ -0,0 +1,18 @@
+/*
+ * program_10_01.c
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int count = 10, x;
+	int *int_pointer;
+
+	int_pointer = &count;
+	x = * int_pointer;
+
+	printf ("count = %i, x = %i\n", count, x);
+
+	return 0;
+}
