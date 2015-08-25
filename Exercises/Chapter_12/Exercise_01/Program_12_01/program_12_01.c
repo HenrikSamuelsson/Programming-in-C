2c9d812dbf3ff1692bbd78c9f0b0468f857007e6
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-08-25 08:21:34 +0200

    Adds source code for Program 12.1.

---
+++ Exercises/Chapter_12/Exercise_01/Program_12_01/program_12_01.c
@@ -0,0 +1,42 @@
+/*
+ * program_12_01.c - Introducing the #define statement
+ */
+
+#include <stdio.h>
+
+#define YES		1
+#define NO		0
+
+/*
+ *  Function to determine if an integer is even.
+ */
+
+int isEven (int number)
+{
+	int answer;
+
+	if ( number % 2 == 0 )
+		answer = YES;
+	else
+		answer = NO;
+
+	return answer;
+}
+
+int main (void)
+{
+	int isEven (int number);
+
+	if ( isEven (17) == YES )
+		printf ("yes ");
+	else
+		printf ("no ");
+
+
+	if ( isEven (20) == YES )
+		printf ("yes\n");
+	else
+		printf ("no\n");
+
+	return 0;
+}
