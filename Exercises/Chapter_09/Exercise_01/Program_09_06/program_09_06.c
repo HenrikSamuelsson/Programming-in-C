6abc4f3454a3b161fb8f7cac575f1c2460b83b6c
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-09-14 22:02:42 +0200

    Adds Program 9.6.

---
+++ Exercises/Chapter_09/Exercise_01/Program_09_06/program_09_06.c
@@ -0,0 +1,38 @@
+/*
+ * program_09_06.c - Reading Lines of Data
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	int i;
+	char line[81];
+	void readLine (char buffer[]);
+
+	for ( i = 0; i < 3; ++i)
+	{
+		readLine (line);
+		printf ("%s\n\n", line);
+	}
+
+	return 0;
+}
+
+/*
+ * Function to read a line of text from the terminal.
+ */
+void readLine (char buffer[])
+{
+	char character;
+	int i = 0;
+
+	do
+	{
+		character = getchar ();
+		buffer[i] = character;
+		++i;
+	}
+	while ( character != '\n' );
+
+	buffer[i - 1] = '\0';
+}
