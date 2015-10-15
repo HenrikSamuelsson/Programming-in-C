bb2926d4922bb6e61164d9bc7bd3094370f49462
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-10-15 22:51:22 +0200

    Adds source code for Program 10.8.

---
+++ Exercises/Chapter_10/Exercise_01/Program_10_08/program_10_08.c
@@ -0,0 +1,24 @@
+/*
+ * program_10_08.c
+ */
+
+#include <stdio.h>
+
+void test (int *int_pointer)
+{
+	*int_pointer = 100;
+}
+
+int main (void)
+{
+	void test (int *int_pointer);
+	int i = 50, *p = &i;
+
+	printf ("Before the call to test i = %i\n", i);
+
+	test (p);
+
+	printf ("After the call to test i = %i\n", i);
+
+	return 0;
+}
