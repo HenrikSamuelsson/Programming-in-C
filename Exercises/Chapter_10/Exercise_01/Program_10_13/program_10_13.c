b90cedcaf9e15b592d61b000fe98ea54b76622b6
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-10-20 23:07:13 +0200

    Adds source code for Program 10.13.

---
+++ Exercises/Chapter_10/Exercise_01/Program_10_13/program_10_13.c
@@ -0,0 +1,28 @@
+/*
+ * program_10_13.c - Pointer version of copyString()
+ */
+
+#include <stdio.h>
+
+void copyString (char *to, char *from)
+{
+	for ( ; *from != '\0'; ++from, ++to )
+		*to = *from;
+
+	*to = '\0';
+}
+
+int main (void)
+{
+	void copyString (char *to, char *from);
+	char string1[] = "A string to be copied.";
+	char string2[50];
+
+	copyString (string2, string1);
+	printf ("%s\n", string2);
+
+	copyString (string2, "So is this.");
+	printf ("%s\n", string2);
+
+	return 0;
+}
