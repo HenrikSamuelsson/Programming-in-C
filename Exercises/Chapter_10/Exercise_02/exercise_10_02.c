2f0a67d830179653a1ef000eb65381116471e5e9
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-10-21 23:08:53 +0200

    Adds source code for exercise 10.2.

---
+++ Exercises/Chapter_10/Exercise_02/exercise_10_02.c
@@ -0,0 +1,57 @@
+/*
+ * exercise_10_02.c
+ */
+
+#include <stdio.h>
+
+struct entry
+{
+	int value;
+	struct entry *next;
+};
+
+void insertEntry(struct entry *newEntry, struct entry *existingEntry)
+{
+	newEntry->next = existingEntry->next;
+	existingEntry->next = newEntry;
+}
+
+int main (void)
+{
+	void insertEntry(struct entry *newEntry, struct entry *existingEntry);
+
+	struct entry n1, n2, nE;
+	struct entry *listPointer;
+
+	n1.value = 100;
+	n1.next = &n2;
+
+	nE.value = 250;
+
+	n2.value = 200;
+	n2.next = (struct entry *) 0;	// mark list end with null pointer
+
+	listPointer = &n1;
+
+	printf("Values in the list before insert of new entry: ");
+	while ( listPointer != (struct entry *) 0 )
+	{
+			printf ("%i ", listPointer->value);
+			listPointer = listPointer->next;
+	}
+	printf("\n");
+
+	insertEntry(&nE, &n1);
+
+	listPointer = &n1;
+
+	printf("Values in the list after insert of new entry: ");
+	while ( listPointer != (struct entry *) 0 )
+	{
+		printf ("%i ", listPointer->value);
+		listPointer = listPointer->next;
+	}
+	printf("\n");
+
+	return 0;
+}
