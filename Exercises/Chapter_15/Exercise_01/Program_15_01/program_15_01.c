eb821eca9eaa3d9b6d52f94a190bca06cd1bd7e5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-10-28 22:20:35 +0100

    Fix of indentation.

--- Exercises/Chapter_15/Exercise_01/Program_15_01/program_15_01.c
+++ Exercises/Chapter_15/Exercise_01/Program_15_01/program_15_01.c
@@ -6,18 +6,18 @@
 
 int main (void)
 {
-	char			c = 'X';
-	char			s[] = "abcdefghijklmnopqrstuvwxyz";
-	int 			i = 425;
-	short int 		j = 17;
-	unsigned int	u = 0xf179U;
-	long int		l = 75000L;
-	long long int	L = 0x1234567812345678LL;
-	float			f = 12.978F;
-	double			d = -97.4583;
-	char			*cp = &c;
-	int 			*ip = &i;
-	int				c1, c2;
+	char            c = 'X';
+	char            s[] = "abcdefghijklmnopqrstuvwxyz";
+	int             i = 425;
+	short int       j = 17;
+	unsigned int    u = 0xf179U;
+	long int        l = 75000L;
+	long long int   L = 0x1234567812345678LL;
+	float           f = 12.978F;
+	double          d = -97.4583;
+	char            *cp = &c;
+	int             *ip = &i;
+	int             c1, c2;
 
 	printf ("Integers:\n");
 	printf ("%i %o %x %u\n", i ,i ,i, i);
